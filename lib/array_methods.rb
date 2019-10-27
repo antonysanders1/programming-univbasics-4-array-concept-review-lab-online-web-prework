369e3af18f2fbd1cff2f5b1c79d5d5884f5cef94
Antony Sanders <antony.devon92@gmail.com>
2019-10-27 16:07:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,8 +9,14 @@ end
 
 def find_max_value(array)
   # Add your solution here
+  
+  array.find_max_value
+  
 end
 
 def find_min_value(array)
   # Add your solution here
+  
+  array.find_min_value
+  
 end

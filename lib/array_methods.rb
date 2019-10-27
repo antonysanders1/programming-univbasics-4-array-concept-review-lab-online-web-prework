f48f2b60da2abd9af82c03293bb3274fa6bdff23
Antony Sanders <antony.devon92@gmail.com>
2019-10-27 16:17:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,20 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+   found_index = nil #initialize a found_index variable
+  counter = 0
   
-  array.find_index(value_to_find)
+  while counter < array.size do
+    
+    ## here we are looping through the items in the array.
+    ## we also have access to the index (counter) each time around the loop.
+    ## if we find the "value_to_find", we want to store the index at which we 
+    ## found it in our variable
+    
+    counter += 1
+  end
   
   
-  
+  ## return found_index
 end
 
 def find_max_value(array)

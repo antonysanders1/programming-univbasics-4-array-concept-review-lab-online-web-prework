aa96cd89f1784d6ad21b2e76ca9d5508b29b9fde
Antony Sanders <antony.devon92@gmail.com>
2019-10-27 16:18:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,7 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
-   found_index = nil #initialize a found_index variable
+  
+  found_index = nil #initialize a found_index variable
   counter = 0
   
   while counter < array.size do
